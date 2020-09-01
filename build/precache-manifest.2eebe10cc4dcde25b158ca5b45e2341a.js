b38d0e561869c4632c49ce0887321f5ccabdccad
biswa17 <b.r.lenka17@gmail.com>
2020-09-01 15:50:17 +0530

    deploy commit

---
+++ build/precache-manifest.2eebe10cc4dcde25b158ca5b45e2341a.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b07bd72fc59ba6da166f6db706260996",
+    "url": "/index.html"
+  },
+  {
+    "revision": "a45145f956963862e048",
+    "url": "/static/css/main.53bfaa92.chunk.css"
+  },
+  {
+    "revision": "ec12916a3f78b8a25b92",
+    "url": "/static/js/2.4acc2d8d.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.4acc2d8d.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "a45145f956963862e048",
+    "url": "/static/js/main.7d2ae6f0.chunk.js"
+  },
+  {
+    "revision": "969ec6062bb99b0b9815",
+    "url": "/static/js/runtime-main.a54c6ea9.js"
+  }
+]);
